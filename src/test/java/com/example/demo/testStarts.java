5aaba2704a408042f33eadc8982d35a092532636
mahiralkan <mahir.alkan.100@gmail.com>
2022-03-29 12:50:24 -0600

    commit 6

--- src/test/java/com/example/demo/testStarts.java
+++ src/test/java/com/example/demo/testStarts.java
@@ -5,6 +5,7 @@ public class testStarts {
     public static void main(String[] args) {
         System.out.println("new");
         System.out.println("new22");
+        System.out.println("nn");
 
     }
 }
